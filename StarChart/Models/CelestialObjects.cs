4e21ee51df33b1670884c172cde6694fd4c3b7a5
tefriedman <tefriedman@hotmail.com>
2021-09-24 18:00:25 -0500

    renamed CelestialObject to Celestial Objects

--- StarChart/Models/CelestialObjects.cs
+++ StarChart/Models/CelestialObjects.cs
@@ -7,14 +7,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StarChart.Models
 {
-    public class CelestialObject
+    public class CelestialObjects
     {
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
         [NotMapped]
-        public List<CelestialObject> Satellites { get; set; }
+        public List<CelestialObjects> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
 
 

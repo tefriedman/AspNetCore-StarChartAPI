2fc3e51c117a7140133d7c48f4b8b1bd969e80c4
tefriedman <tefriedman@hotmail.com>
2021-09-28 14:03:59 -0500

    created attributes, context field and constructor

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
 
 namespace StarChart.Controllers
 {
+    [ApiController]
+    [Route("")]
     public class CelestialObjectController:ControllerBase
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
     }
 }
